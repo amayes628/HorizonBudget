0e310076c23fb01824def9cf2211d07113923443
amayes628 <amayes@live.ca>
2026-08-01 08:24:38 -0700

    VIM magic conflict resolved

--- HorizonBudget/GlobalUsings.cs
+++ HorizonBudget/GlobalUsings.cs
@@ -1,7 +1,8 @@
 global using System.Collections.Immutable;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
 global using HorizonBudget.Models;
 global using HorizonBudget.Presentation;
-global using HorizonBudget.Services.Endpoints;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Localization;
---
+++ HorizonBudget/Presentation/SecondViewModel.cs
@@ -0,0 +1,5 @@
+namespace HorizonBudget.Presentation;
+
+public partial record SecondViewModel(Entity Entity)
+{
+}
