44bd1436ab06aead99758d9380aa8eb3cdb00b98
amayes628 <amayes@live.ca>
2026-08-02 06:10:20 -0700

    Huston we have liftoff of HorizonBudget

--- HorizonBudget.Data/Migrations/20260802124832_PendingChanges.Designer.cs
+++ HorizonBudget.Data/Migrations/20260802124832_PendingChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HorizonBudget.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HorizonBudget.Data.Migrations
 {
     [DbContext(typeof(HorizonBudgetContext))]
-    partial class HorizonBudgetContextModelSnapshot : ModelSnapshot
+    [Migration("20260802124832_PendingChanges")]
+    partial class PendingChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.10");
---
+++ HorizonBudget.Data/Migrations/20260802124832_PendingChanges.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HorizonBudget.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Accounts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<uint>(
+                name: "CategoryId",
+                table: "Expenses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0u);
+
+            migrationBuilder.AddColumn<uint>(
+                name: "CategoryId",
+                table: "Accounts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0u);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
